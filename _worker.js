13646b3167b525e32a6d2168f8d47349067dc4a9
cody33231 <cody_chen@163.com>
2024-09-02 13:13:51 +0800

    Update _worker.js
    update userID

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '57841be2-518f-4d5f-99e4-d8719ee34f65';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
